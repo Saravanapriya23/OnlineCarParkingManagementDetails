2e171d88a98c65ed58e5fd8a1c57eaa0dd2f9888
Saravanapriya23 <58771918+Saravanapriya23@users.noreply.github.com>
2020-02-20 00:49:56 +0530

    Add files via upload

---
+++ OnlineCarParkingManagementDetails.Entity/CarOwnerDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineCarParkingManagementDetails.Entity
+{
+    public enum Role
+    {
+        Customer,
+        CarParkingSiteOwner
+    }
+    public class CarOwnerDetails
+    {
+        
+        public string name { get; set; }
+        public string gender { get; set; }
+        public long mobileNo { get; set; }
+        public string address { get; set; }
+        public string emailId { get; set; }
+        public string password { get; set; }
+        public string conformPassword { get; set; }
+        public Role UserRole { get; set; }
+        public CarOwnerDetails()
+        {
+
+        }
+        public CarOwnerDetails(string Name, string Gender,long MobileNo, string location,string EmailId,string Password,string conformpassword,Role userrole)
+        {
+            name = name;
+            gender = Gender;
+            mobileNo = MobileNo;
+            address = location;
+            emailId = EmailId;
+            password = Password;
+            conformPassword = conformpassword;
+            UserRole = userrole;
+        }
+    }
+}
---
+++ OnlineCarParkingManagementDetails.Repository/CarOwnerDetailsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using OnlineCarParkingManagementDetails.Entity;
+namespace OnlineCarParkingManagementDetails.Repository
+{
+    public class CarOwnerDetailsRepository
+    {
+        public static List<CarOwnerDetails> carOwnerDetails = new List<CarOwnerDetails>();
+        static CarOwnerDetailsRepository()
+        {
+            carOwnerDetails.Add(new CarOwnerDetails("Sara","Female",8778636133,"Chennai","sara@gmail.com","special*23","special*23", Role.Customer));
+            carOwnerDetails.Add(new CarOwnerDetails("Priya","Female",9865514335,"Pune","priya@gmail.com","priya@","priya@",Role.CarParkingSiteOwner));
+            carOwnerDetails.Add(new CarOwnerDetails("Jahnavi","Female",7094410201,"Bangalore","jahnavi@gmail.com","jahnavi*","jahnavi*",Role.Customer));
+        }
+        public IEnumerable<CarOwnerDetails> GetCarOwnerDetails()
+        {
+            return carOwnerDetails;
+        }
+        public static void SignUp(CarOwnerDetails carOwnerInfo)
+        {
+            carOwnerDetails.Add(carOwnerInfo);
+        }
+    }
+}
